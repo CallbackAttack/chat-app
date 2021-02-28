d84dac5fdcee1f2d14839672d2aca5525d6cc37c
Yan <yan.doschinskiy@gmail.com>
2021-02-28 16:55:01 +0700

    eslint+prettier --dev added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
@@ -8,19 +9,20 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          sadfasdfsdafsadfsdafdsaf
+          sadfasdfsdafsadfsdafdsafsadfsdaffdsafads
         </p>
         <a
+          id="element"
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React sadfsdafsadfds
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
